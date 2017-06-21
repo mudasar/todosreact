bfc3ba041419a36e7e239d98ad8fcee612324c25
Mudasar Rauf <mudassar.rauf@gmail.com>
2017-06-21 12:57:36 +0100

    initialized basics

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 
 export const Header = (props) => {
-    return(
+    return (
         <div>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="dropdown menu" data-dropdown-menu>
+                        <li className="menu-text">Todo App</li>
+                        <li>
+                            <NavLink exact to="/" activeClassName="button" >Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact" activeClassName="button">Contact</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" activeClassName="button">About</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <ul className="menu">
+                        <li><input type="search" placeholder="Search"/></li>
+                        <li>
+                            <button type="button" className="button">Search</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             {props.children}
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/foundation-sites/dist/css/foundation.min.css';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Foundation from 'react-foundation';
